558545b45044ead8dd17d348e2b65758884e66b6
Joel Faubert <joel.faubert@snowedin.ca>
2023-04-28 17:55:52 -0400

    Added device id to input motions bound in ArcadeInputMap singleton

--- game/auto/ArcadeInputMap.gd
+++ game/auto/ArcadeInputMap.gd
@@ -47,49 +47,50 @@ func create_input_map(id : int):
 
 	print("Creating device mapping for id %d" % id) 
 	InputMap.add_action(horizontal_axis_action % id)
-	InputMap.action_add_event(horizontal_axis_action % id, InputEventJoypadMotion.new())
+	var horizontal_axis_joypad_motion = InputEventJoypadMotion.new()
+	horizontal_axis_joypad_motion.device = id
+	horizontal_axis_joypad_motion.axis = 0
+	InputMap.action_add_event(horizontal_axis_action % id, horizontal_axis_joypad_motion)
 	
 	InputMap.add_action(vertical_axis_action % id)
 	var vertical_axis_joypad_motion = InputEventJoypadMotion.new()
+	vertical_axis_joypad_motion.device = id
 	vertical_axis_joypad_motion.axis = 1
 	InputMap.action_add_event(vertical_axis_action % id, vertical_axis_joypad_motion)
 	
 	InputMap.add_action(a_button_action % id)
 	var a_button = InputEventJoypadButton.new()
+	a_button.device = id
 	a_button.button_index = JOY_BUTTON_A
 	InputMap.action_add_event(a_button_action % id, a_button)
 	
 	InputMap.add_action(b_button_action % id)
 	var b_button = InputEventJoypadButton.new()
+	b_button.device = id
 	b_button.button_index = JOY_BUTTON_B
 	InputMap.action_add_event(b_button_action % id, b_button)
 	
 	InputMap.add_action(x_button_action % id)
 	var x_button = InputEventJoypadButton.new()
+	x_button.device = id
 	x_button.button_index = JOY_BUTTON_X
 	InputMap.action_add_event(x_button_action % id, x_button)
 	
 	InputMap.add_action(y_button_action % id)
 	var y_button = InputEventJoypadButton.new()
+	y_button.device = id
 	y_button.button_index = JOY_BUTTON_Y
 	InputMap.action_add_event(y_button_action % id, y_button)
 
 
 func remove_input_map(id : int):
-	if InputMap.has_action(horizontal_axis_action % id):
-		InputMap.erase_action(horizontal_axis_action % id)
-	
-	if InputMap.has_action(vertical_axis_action % id):
-		InputMap.erase_action(vertical_axis_action % id)
-	
-	if InputMap.has_action(a_button_action % id):
-		InputMap.erase_action(a_button_action % id)
-	
-	if InputMap.has_action(b_button_action % id):
-		InputMap.erase_action(b_button_action % id)
-	
-	if InputMap.has_action(x_button_action % id):
-		InputMap.erase_action(x_button_action % id)
-	
-	if InputMap.has_action(y_button_action % id):
-		InputMap.erase_action(y_button_action % id)
+	for action in actions:
+		if not InputMap.has_action(action % id):
+			print("Failed to remove input map for device %d: action does not exist" % id)
+			return
+	InputMap.erase_action(horizontal_axis_action % id)
+	InputMap.erase_action(vertical_axis_action % id)
+	InputMap.erase_action(a_button_action % id)
+	InputMap.erase_action(b_button_action % id)
+	InputMap.erase_action(x_button_action % id)
+	InputMap.erase_action(y_button_action % id)
