17c598da65ba5bc4b3b5f6b83cd86d68cb153efc
faubes <joelfaubert@gmail.com>
2023-04-29 20:51:51 -0400

    Longer range, better rotation handling, retract if stuck after collision

--- game/scenes/player/Claw.gd
+++ game/scenes/player/Claw.gd
@@ -3,15 +3,8 @@ class_name Claw
 
 @onready var animation_player = $AnimationPlayer
 
-
-var target : Vector2 = Vector2.RIGHT
-
 func _ready():
 	animation_player.play_backwards("open")
-
-func _process(delta):
-	#look_at(target)
-	pass
 
 func open() -> void : 
 	if animation_player:
@@ -21,3 +14,7 @@ func close() -> void :
 	if animation_player:
 		animation_player.play_backwards("open")
 		
+
+func _on_grab_area_body_entered(body):
+	if body:
+		print("Body {name} entered {this}".format({"name" : body.name, "this" : self.name}))
--- game/scenes/player/Player.gd
+++ game/scenes/player/Player.gd
@@ -18,11 +18,20 @@ enum arm_state { retracted, extending, retracting }
 var current_hand_state : hand_state = hand_state.closed
 var current_arm_state : arm_state = arm_state.retracted
 
+var switch_rotation : bool = false
+var current_rotation_input : float = 0
+var target_angle : float
+var player_flip_sign = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if player_id == 2 or player_id == 3:
+		player_flip_sign = -1
+	
 	claw.global_position = self.global_position + min_range * Vector2.RIGHT
 	print("{player} {angle}".format({"player" : player_id, "angle" : phase_angle}))
-	#rotation = phase_angle
+	target_angle = phase_angle
+	rotation = target_angle
 	
 
 func set_arm_state(new_state : arm_state):
@@ -32,19 +41,35 @@ func set_arm_state(new_state : arm_state):
 	current_arm_state = new_state
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _physics_process(delta):
 	match current_arm_state:
+		arm_state.retracted:
+			claw.linear_velocity = Vector2.ZERO
 		arm_state.extending:
 			if claw.global_position.distance_to(self.global_position) > groove_joint_2d.length * 0.9:
 				claw.linear_velocity = Vector2.ZERO
-				claw.apply_impulse(retract_speed * transform.basis_xform(Vector2.DOWN))
 				set_arm_state(arm_state.retracting)
+			elif claw.linear_velocity.length() < 1 and claw.global_position.distance_to(self.global_position) > groove_joint_2d.length * 0.2:
+				set_arm_state(arm_state.retracting)
+			else:
+				claw.apply_force(speed  * transform.basis_xform(Vector2.DOWN))
 		arm_state.retracting:
 			if claw.global_position.distance_to(self.global_position) < min_range:
 				set_arm_state(arm_state.retracted)
 				claw.linear_velocity = Vector2.ZERO
-				
+			else:
+				claw.apply_force(-retract_speed * transform.basis_xform(Vector2.DOWN))
+
+	if switch_rotation:
+		var remap_angle = (current_rotation_input + 1) / 2
+		target_angle = lerp_angle(phase_angle, phase_angle + PI/2,  remap_angle)
+		switch_rotation = false
 	
+	if (not is_equal_approx(rotation, target_angle)):
+		rotation = move_toward(rotation, target_angle, delta)
+
+
+
 func _input(event):
 	if event.device != player_id:
 		return
@@ -59,17 +84,17 @@ func _input(event):
 				claw.open()
 				current_hand_state = hand_state.open
 
+		
 	match current_arm_state:
 		arm_state.retracted:
-			if event.is_action_pressed("TiltClockwise"):
-				var new_rotation = clampf(rotation - 0.1, phase_angle, phase_angle + PI/2)
-				rotation = new_rotation
-			if event.is_action_pressed("TiltCounterclockwise"):
-				var new_rotation = clampf(rotation + 0.1, phase_angle, phase_angle + PI/2)
-				rotation = new_rotation
-			
 			if event.is_action_pressed("FireHand"):
-				print("retracted")
+				print("FireHand")
 				claw.linear_velocity = Vector2.ZERO
-				claw.apply_impulse(speed  * transform.basis_xform(Vector2.DOWN))
 				set_arm_state(arm_state.extending)
+				target_angle = rotation # stop rotating
+			if event.is_action_pressed("TiltClockwise"):
+				switch_rotation = true
+				current_rotation_input = 1 * player_flip_sign
+			if event.is_action_pressed("TiltCounterclockwise"):
+				switch_rotation = true
+				current_rotation_input = -1 * player_flip_sign
